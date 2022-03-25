7757e02b35e8f390b6dbc056dce3c08b4ca69b1b
Marsel <chelovek-gorod@yandex.by>
2022-03-25 09:30:57 +0300

    add img js
    set image as div ИП with 100% size

---
+++ js/img.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const MAIN = document.querySelector('main');
+const ImagesArr = Array.from(MAIN.querySelectorAll('img'));
+let imagesArrSize = ImagesArr.length;
+
+ImagesArr.forEach(img => selectImageType(img));
+
+function selectImageType(img) {
+
+    let div = document.createElement('div');
+
+    let imgClass = (
+           img.className === 'full-width'
+        || img.className === 'float-left'
+        || img.className === 'float-right')
+        ?  img.className : 'class-error';
+
+    div.className = imgClass; console.log('imgClass =', imgClass);
+
+    if(imgClass === 'class-error') div.innerHTML = `
+        ERROR in image class name<br><br>
+        class name <b><i>${img.className}</i></b> not supported`;
+    else {
+        div.style.backgroundImage = `url(${img.src})`;
+        //  if (img.clientWidth > img.clientHeight) 
+    }
+
+    MAIN.insertBefore(div, img);
+    img.remove();
+}
